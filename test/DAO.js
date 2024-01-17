4b6d49484df708c254fec65bc3a6e8e38da274d3
Ben Castilla <ben@castilla.us>
2024-01-17 08:19:55 -0800

    Added Voting

--- test/DAO.js
+++ test/DAO.js
@@ -39,27 +39,26 @@ describe('DAO', () => {
     const Token = await ethers.getContractFactory('Token')
     token = await Token.deploy('Dapp University', 'DAPP', '1000000')
 
-    // Send tokens to investors - each one gets 20%
-    transaction  = await token.connect(deployer).transfer(investor1.address, tokens(200000))
+    transaction = await token.connect(deployer).transfer(investor1.address, tokens(200000))
     await transaction.wait()
 
-   transaction  = await token.connect(deployer).transfer(investor2.address, tokens(200000))
+    transaction = await token.connect(deployer).transfer(investor2.address, tokens(200000))
     await transaction.wait()
 
-    transaction  = await token.connect(deployer).transfer(investor3.address, tokens(200000))
+    transaction = await token.connect(deployer).transfer(investor3.address, tokens(200000))
     await transaction.wait()
 
-    transaction  = await token.connect(deployer).transfer(investor4.address, tokens(200000))
+    transaction = await token.connect(deployer).transfer(investor4.address, tokens(200000))
     await transaction.wait()
 
-    transaction  = await token.connect(deployer).transfer(investor5.address, tokens(200000))
+    transaction = await token.connect(deployer).transfer(investor5.address, tokens(200000))
     await transaction.wait()
 
     // Deploy DAO
     // Set Quorum to > 50% of token total supply, 
-    // 500k tokens + 1 wei, i.e., 5000000000000000000000001
+    // 500k tokens + 1 wei, i.e., 500000000000000000000001
     const DAO = await ethers.getContractFactory('DAO')
-    dao = await DAO.deploy(token.address, '5000000000000000000000001') 
+    dao = await DAO.deploy(token.address, '500000000000000000000001') 
 
     // Funder sends 100 Ether to DAO treasury for Governance
     await funder.sendTransaction({ to: dao.address, value: ether(100) })
@@ -76,7 +75,7 @@ describe('DAO', () => {
     })
 
     it('returns quorum', async () => {
-      expect(await dao.quorum()).to.equal('5000000000000000000000001')
+      expect(await dao.quorum()).to.equal('500000000000000000000001')
     })
 
   })
@@ -85,6 +84,7 @@ describe('DAO', () => {
     let transaction, result
 
     describe('Success', () => {
+
         beforeEach(async () => {
           transaction = await dao.connect(investor1).createProposal('Proposal 1', ether(100), recipient.address)
           result = await transaction.wait()
@@ -118,18 +118,53 @@ describe('DAO', () => {
         it('reject non-investor', async () => {
 await expect(dao.connect(user).createProposal('Proposal 1', ether(1000), recipient.address)).to.be.reverted
         })
+      })
+    })
 
-        it('', async () => {
+  describe('Voting', () => {
+    let transaction, result
 
+      beforeEach(async () => {
+        transaction = await dao.connect(investor1).createProposal('Proposal 1', ether(100), recipient.address)
+        result = await transaction.wait()
+      })
+
+
+    describe('Success', () => {
+
+        beforeEach(async () => {
+          transaction = await dao.connect(investor1).vote(1)
+          result = await transaction.wait()
+        })
+
+        it('updates vote count', async () => {
+          const proposal = await dao.proposals(1)
+          expect(proposal.votes).to.equal(tokens(200000))
+        })
+
+        it('emits vote event', async () => {
+          await expect(transaction).to.emit(dao, "Vote")
+           .withArgs(1, investor1.address)
         })
 
 
     })
 
+    describe('Failure', () => {
+
+        it('reject non-investor', async () => {
+          await expect(dao.connect(user).vote(1)).to.be.reverted
+        })
+
+        it('rejects double voting', async () => {
+          transaction = await dao.connect(investor1).vote(1)
+          await transaction.wait()
+          
+          await expect(dao.connect(user).vote(1)).to.be.reverted
+
+        })
 
 
-
-  })
-
-
+      })
+    })
 })
